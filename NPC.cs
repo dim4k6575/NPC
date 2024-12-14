291296b953aaf09ef5942e16fe2b20140337fde4
dim4k6575 <bobrova171980@gmail.com>
2024-12-14 18:32:33 +0300

    NPC.cs

---
+++ NPC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{   
+    int health = 5;
+    int level = 1;
+    float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {   health += level;
+        print (health);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
